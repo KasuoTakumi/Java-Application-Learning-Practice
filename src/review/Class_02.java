2c2770399ffa88aa83800469fff5c5e72098ef6b
Kso <kasuo.takumi@gmail.com>
2018-02-20 19:58:40 +0900

    インスタンス呼び出しの練習  引数を渡す練習

---
+++ src/review/Class_02.java
@@ -0,0 +1,23 @@
+package review;
+
+public class Class_02 {
+
+	public static void counter(int count) {
+
+		if(count >= 0) {
+			for(int i=1; i<=count; i++) {
+				System.out.println("count "+i);
+
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		counter(10);
+
+	}
+
+}
